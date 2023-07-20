f96359512fa4e930f62c377c679ce4ab6dd00409
Minsup Lee <m1nsuppp@gmail.com>
2023-07-21 02:35:49 +0900

    refactor: 상수임을 표시하고, value들은 변경될 수 있음을 표시

--- src/components/LoginForm/validationRules.ts
+++ src/components/LoginForm/validationRules.ts
@@ -7,46 +7,47 @@ type LoginFormRegisterOptions = Pick<
   'maxLength' | 'minLength' | 'required' | 'pattern'
 >;
 
-export const LoginFormValidationRules: Record<Field, LoginFormRegisterOptions> =
-  {
-    ID: {
-      required: {
-        value: true,
-        message: 'ID는 필수 입력사항입니다.',
-      },
-      minLength: {
-        value: 6,
-        message: 'ID는 최소 6자 이상입니다. ID를 다시 확인해주세요.',
-      },
-      maxLength: {
-        value: 30,
-        message: 'ID는 최대 30자입니다. ID를 다시 확인해주세요.',
-      },
-      pattern: {
-        value: /^[a-z0-9]+$/,
-        message:
-          'ID는 영어 소문자와 숫자로만 이루어집니다. ID를 다시 확인해주세요.',
-      },
+export const LOGIN_FORM_VALIDATION_RULES: Record<
+  Field,
+  LoginFormRegisterOptions
+> = {
+  ID: {
+    required: {
+      value: true,
+      message: 'ID는 필수 입력사항입니다.',
     },
-    PASSWORD: {
-      required: {
-        value: true,
-        message: '비밀번호는 필수 입력사항입니다.',
-      },
-      minLength: {
-        value: 12,
-        message:
-          '비밀번호는 최소 12자 이상입니다. 비밀번호를 다시 확인해주세요.',
-      },
-      pattern: {
-        value:
-          /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\\|[\]{};:'",.<>/?]).+$/,
-        message:
-          '비밀번호는 영어 대소문자, 숫자, 특수문자를 적어도 하나씩 포함해야합니다. 비밀번호를 다시 확인해주세요.',
-      },
-      maxLength: {
-        value: 60,
-        message: '비밀번호는 최대 60자입니다. 비밀번호를 다시 확인해주세요.',
-      },
+    minLength: {
+      value: 6,
+      message: 'ID는 최소 6자 이상입니다. ID를 다시 확인해주세요.',
     },
-  };
+    maxLength: {
+      value: 30,
+      message: 'ID는 최대 30자입니다. ID를 다시 확인해주세요.',
+    },
+    pattern: {
+      value: /^[a-z0-9]+$/,
+      message:
+        'ID는 영어 소문자와 숫자로만 이루어집니다. ID를 다시 확인해주세요.',
+    },
+  },
+  PASSWORD: {
+    required: {
+      value: true,
+      message: '비밀번호는 필수 입력사항입니다.',
+    },
+    minLength: {
+      value: 12,
+      message: '비밀번호는 최소 12자 이상입니다. 비밀번호를 다시 확인해주세요.',
+    },
+    pattern: {
+      value:
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\\|[\]{};:'",.<>/?]).+$/,
+      message:
+        '비밀번호는 영어 대소문자, 숫자, 특수문자를 적어도 하나씩 포함해야합니다. 비밀번호를 다시 확인해주세요.',
+    },
+    maxLength: {
+      value: 60,
+      message: '비밀번호는 최대 60자입니다. 비밀번호를 다시 확인해주세요.',
+    },
+  },
+};
