74a41f66e9a52f60aa6ec2b654ecf60ca85a5e99
Minsup Lee <m1nsuppp@gmail.com>
2023-07-22 18:36:38 +0900

    refactor: `Record`로 수정

--- src/components/LoginForm/data.ts
+++ src/components/LoginForm/data.ts
@@ -1,6 +1,6 @@
 export const ID = 'formiscrazy123';
 export const PASSWORD = '7@2aD5$9Gt!f';
-export const ON_SUBMIT_MESSAGE = {
-  VALID: '로그인 성공!',
-  INVALID: '로그인 실패!',
+export const ON_SUBMIT_MESSAGE: Record<number, string> = {
+  200: '로그인 성공!',
+  400: '로그인 실패!',
 };
