6bd77b391de3094ff166372ec1c25738d7e19dc0
Minsup Lee <m1nsuppp@gmail.com>
2023-09-22 00:34:52 +0900

    feat: init app

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const Pathname = {
+  HOME_PAGE: '/',
+  LOGIN_PAGE: '/login',
+  KAKAO_AUTH: '/api/auth/callback',
+} as const;
---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
