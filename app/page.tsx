b7678bdbf22940258535f372a0810c06fb0ba6df
Minsup Lee <m1nsuppp@gmail.com>
2023-09-22 00:41:43 +0900

    fix: dynamic server usage error

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Pathname } from '@/lib/constants';
 import { auth } from '@/services/auth';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const { session } = await auth();
 
