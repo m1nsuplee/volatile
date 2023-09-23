dbc3eceab5c36a36735ec9fbfe45b860d909166e
Minsup Lee <m1nsuppp@gmail.com>
2023-09-23 22:28:58 +0900

    feat: serverside fetch

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ import clsx from 'clsx';
 import { TodoAddButton } from '@/components/todo-add-button';
 import { TodoList } from '@/components/todo-list';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/db';
 import { cookies } from 'next/headers';
 import { supabaseKey, supabaseUrl } from '@/lib/config';
+import { Database } from '@/types';
+import { getQueryClient } from '@/lib/get-query-client';
+import { Hydrate, dehydrate } from '@tanstack/react-query';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
+  const queryClient = getQueryClient();
+
   const service = createServerComponentClient<Database>(
     {
       cookies,
@@ -24,6 +28,20 @@ export default async function Home() {
   const {
     data: { session },
   } = await service.auth.getSession();
+
+  const fetchTodos = async () => {
+    const { data } = await service
+      .from('todos')
+      .select('*')
+      .eq('user_id', session?.user.id || '')
+      .order('id', { ascending: false });
+
+    return data;
+  };
+
+  await queryClient.prefetchQuery(['todos'], fetchTodos);
+
+  const dehydratedState = dehydrate(queryClient);
 
   if (!session) {
     redirect(Pathname.LOGIN_PAGE);
@@ -46,7 +64,9 @@ export default async function Home() {
           <TodoAddButton session={session} />
         </nav>
       </header>
-      <TodoList userId={session.user.id} />
+      <Hydrate state={dehydratedState}>
+        <TodoList userId={session.user.id} />
+      </Hydrate>
     </main>
   );
 }
