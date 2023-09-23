a30d21760f8c8648c3a6372d0a3ead2580c89447
Minsup Lee <m1nsuppp@gmail.com>
2023-09-23 23:08:04 +0900

    feat: todo 삭제

--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useToggleTodoStatus } from '@/hooks/todo';
+import { useDeleteTodo, useToggleTodoStatus } from '@/hooks/todo';
 import { Todo } from '@/types';
 import { useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
@@ -12,26 +12,43 @@ interface TodoProps {
 export function TodoItem({ todo }: TodoProps) {
   const queryClient = useQueryClient();
 
-  const { mutate: toggleTodoStatus } = useToggleTodoStatus(todo, {
-    onSuccess: () => queryClient.refetchQueries(['todos']),
-  });
+  const { mutate: toggleTodoStatus, isLoading: isToggleTodoStatusLoading } =
+    useToggleTodoStatus(todo, {
+      onSuccess: () => queryClient.refetchQueries(['todos']),
+    });
+
+  const { mutate: deleteTodo, isLoading: isDeleteTodoLoading } = useDeleteTodo(
+    todo.id,
+    {
+      onSuccess: () => queryClient.refetchQueries(['todos']),
+    }
+  );
 
   const handleCompleteCheckboxChange = () => toggleTodoStatus();
+
+  const handleDeleteButtonClick = () => deleteTodo();
 
   return (
     <article
       id={`todo-${todo.id}`}
-      role="article"
+      className={clsx('w-full flex justify-between gap-x-4')}
     >
-      <header className={clsx('w-full flex justify-between gap-x-4')}>
-        <span>{todo.id}</span>
-        <span>{todo.task}</span>
-        <input
-          type="checkbox"
-          checked={todo.is_complete || false}
-          onChange={handleCompleteCheckboxChange}
-        />
-      </header>
+      <span>{todo.id}</span>
+      <span>{todo.task}</span>
+      <input
+        type="checkbox"
+        checked={todo.is_complete || false}
+        onChange={handleCompleteCheckboxChange}
+        disabled={isToggleTodoStatusLoading}
+      />
+      <button
+        type="button"
+        className="text-red-500"
+        onClick={handleDeleteButtonClick}
+        disabled={isDeleteTodoLoading}
+      >
+        (X)
+      </button>
     </article>
   );
 }
